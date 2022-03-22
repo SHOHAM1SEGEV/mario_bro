a44492838fc48bc934b1f35d25e2a6ccb620f123
SHOHAM <shohamys2005@gmail.com>
2022-03-22 20:13:58 +0200

    add photos

--- MAIN.py
+++ MAIN.py
@@ -1,15 +1,31 @@
 import pygame
+from turtledemo import clock
 
 
-def main():
+def MAIN():
     pygame.init()
     pygame.display.set_caption("mario_bro")
 
-    clock = pygame.time.Clock
-    img_path = "images (1).jpg"
-    img = pygame.image.load(img_path)
-    img = pygame.transform.scale(img, (800, 1000))
-    screen = pygame.display.set_mode((800, 1000))
+    # background img (mario photo)
+    x_location = 0
+    y_location = 0
+    width = 800
+    height = 700
+    screen = pygame.display.set_mode((800, 700))
+    img_path_1 = "photos/mario_photo.jpg"
+    img_1 = pygame.image.load(img_path_1)
+    img_1 = pygame.transform.scale(img_1, (width, height))
+    screen.blit(img_1, (x_location, y_location))
+
+    # play button photo
+    x_location = 400
+    y_location = 350
+    width = 200
+    height = 80
+    img_path_2 = "photos/play_button.jpg"
+    img_2 = pygame.image.load(img_path_2)
+    img_2 = pygame.transform.scale(img_2, (width, height))
+    screen.blit(img_2, (x_location, y_location))
 
     running = True
     while running:
@@ -17,11 +33,11 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.blit(img, (0, 0))
+        screen.blit(img_1, (0, 0))
+        screen.blit(img_2, (400, 350))
         pygame.display.update()
-        clock.tick(60)
     pygame.quit()
     quit()
 
 
-main()
+MAIN()
