054c59d6542c2c06a3e7f1d8517cf8372c6193ed
shoha <shohamys2005@gmail.com>
2022-03-24 18:22:54 +0200

    W, H OF SCREEN

--- helpers.py
+++ helpers.py
@@ -1,17 +1,19 @@
 import pygame
 
 # the main screen
-screen = pygame.display.set_mode((800, 700))
+WIDTH_OF_SCREEN = 1290
+HEIGHT_OF_SCREEN = 670
+screen = pygame.display.set_mode((WIDTH_OF_SCREEN, HEIGHT_OF_SCREEN))
 
 # the location of the screen
-# the 1 screen
+# PHOTO OF MARIO
 
 X_LOCATION1 = 0
 Y_LOCATION1 = 0
 WIDTH1 = 800
 HEIGHT1 = 680
 
-# the 2 screen
+# PHOTO OF PLAY BUTTON
 X_LOCATION2 = 275
 Y_LOCATION2 = 300
 WIDTH2 = 200
