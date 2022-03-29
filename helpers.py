4db03bd0cebe92d49abb29e038fbad64894dde40
shoha <shohamys2005@gmail.com>
2022-03-29 19:56:25 +0300

    W, H OF SCREEN

--- helpers.py
+++ helpers.py
@@ -14,13 +14,13 @@ WIDTH1 = 1290
 HEIGHT1 = 670
 
 # PHOTO OF PLAY BUTTON
-X_LOCATION2 = 645
+X_LOCATION2 = 520
 Y_LOCATION2 = 335
 WIDTH2 = 200
 HEIGHT2 = 100
+
 
 def mouse_in_button(button, mouse_pos):
     if button.x_pos + button.width > mouse_pos[0] > button.x_pos and \
             button.y_pos < mouse_pos[1] < button.y_pos + button.height:
         return True
-
