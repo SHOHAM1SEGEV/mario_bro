dd8c7714538fe4c39ad09796875806990b5f2c2b
shoha <shohamys2005@gmail.com>
2022-03-31 17:11:47 +0300

    W, H OF SCREEN

--- Object.py
+++ Object.py
@@ -1,12 +1,10 @@
 import pygame
-
 from helpers import screen
 
 
-class object:
-# Constructor
+class Object:
 def __init__(self, x, y, height, width, pic):
-
+    # Constructor
     self.x = x
     self.y = y
     self.height = height
@@ -14,6 +12,7 @@ def __init__(self, x, y, height, width, pic):
     self.pic = pic
     self.pic = pygame.image.load(self.pic)
     self.image = pygame.transform.scale(pic, (height, width) )
+
 # Method
 def display_image(self):
     screen.blit(self.image, (self.height, self.width))
